4824119442a82f94ceeda0898850bdb19edbef6d
CorentinHouvin <houvin.corentin@gmail.com>
2019-03-28 10:59:02 +0100

    Mise en forme

--- Views/session_detail.php
+++ Views/session_detail.php
@@ -25,32 +25,56 @@
 		$dateDebut = $session->getDateDebut();
 		$dateFin = $session->getDateFin();
 
+		echo '<h1 class="display-4">' . $session->getFormations()->getNom() . ' - #' . $session->getId() . '</h1>';
+
 		echo '<p>';
-		echo 'Numero session : ' . $session->GetId() . '<br>';
 		echo 'Date de début : ' . $dateDebut->format('d/m/Y') . '<br>';
 		echo 'Date de fin : ' . $dateFin->format('d/m/Y') . '<br>';
-		echo 'Nom formation : ' . $session->getFormations()->getNom() . ' <br>';
-		echo 'Nom professeur : ' . $session->getProfesseurs()->getPrenom() . ' '. $session->getProfesseurs()->getNom()  .' <br>';
+		echo 'Responsable de la formation : <a href="./professeur_detail.php?id_professeur=' . $session->getProfesseurs()->getId(). '">' . $session->getProfesseurs()->getPrenom() . ' '. $session->getProfesseurs()->getNom()  .' </a><br>';
 		echo 'Numero de salle : ' . $session->getSalles()->getId() . ' <br>';
 		echo '</p>';
 
-		echo 'Liste des compte rendus : <br>';
-		echo '<ul>';
+		echo '<h3>Liste des compte rendus :</h3>';
+
+		?>
+		
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>Id compte rendus</th>
+                    <th>Professeur</th>
+					<th>Commentaire</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if(empty($results)){ ?>
+					<tr>
+						<td class="text-center" colspan ="6">
+							Aucune session
+						</td>
+					</tr>
+				<?php }
 
 		foreach($results as $compte_rendus)
 		{
 			$compte_rendusId = $compte_rendus->getId();
 			$professeurId = $compte_rendus->getProfesseurs()->getId();
 			$professeur = $entityManager->find('\Professeur', $professeurId);
+			$commentaire = $compte_rendus->getCommentaire();
+			?>
 
-			echo '<li>';
-			echo 'Compte Rendu numero ' . $compte_rendusId;
-			echo '<a href="./compterendu_detail.php?id_compterendu=' . $compte_rendusId . '"> (détail du compte rendu)</a>';
-			echo ' pris en note par <a href="./professeur_detail.php?id_professeur=' . $professeurId . '">' . $professeur->getNom() . ' ' . $professeur->getPrenom() . '</a><br>';
-			echo '</li>';
+			<tr>
+				<td><?= $compte_rendusId; ?></td>
+				<td><a href="./professeur_detail.php?id_professeur=<?= $professeurId ?>"><?= $professeur->getNom() . ' ' . $professeur->getPrenom(); ?></a></td>
+				<td><?= $commentaire; ?></td>
+			</tr>
+
+			<?php
 		}
-
-		echo '</ul>';
+		?>
+			</tbody>
+		</table>
+		<?php
 	}
 
 	$content = ob_get_clean();
