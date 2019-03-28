e0378140315a53f14226071d706607d1b6eed921
CorentinHouvin <houvin.corentin@gmail.com>
2019-03-28 09:55:15 +0100

    Mise en forme

--- Views/entreprises.php
+++ Views/entreprises.php
@@ -7,10 +7,37 @@ require_once '../bootstrap.php';
 	$entreprises = $entityManager->getRepository('\Entreprise')->findAll();
 	$title = 'Liste des entreprises';
 
-	foreach($entreprises as $entreprise)
-	{
-		echo '<a href="./entreprise_detail.php?id_entreprise=' . $entreprise->getId() . '">' . $entreprise->getNom() . '</a><br>';
-	}
+	echo '<h3>Liste des entreprises :</h3>';
+
+	?>
+		
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>Id entreprise</th>
+					<th>Nom entreprise</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if(empty($entreprises)){ ?>
+					<tr>
+						<td class="text-center" colspan ="6">
+							Aucune session
+						</td>
+					</tr>
+				<?php }
+
+				foreach($entreprises as $entreprise)
+				{
+					?>
+					<tr>
+						<td><?= $entreprise->getId(); ?></td>
+						<td><?= $entreprise->getNom(); ?></td>
+						<td><a href="./entreprise_detail.php?id_entreprise=<?= $entreprise->getId(); ?>">Détail</a></td>
+					</tr>
+					<?php
+				}
 
 	$content = ob_get_clean();
 
